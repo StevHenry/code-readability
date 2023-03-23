1119567605e18c9c2a4adaed0626082c99fcbd4c
Sidiplayer <sidi.sid-ahmed@mines-ales.org>
2023-03-23 16:10:59 +0100

    starting pipline

--- pre_processing.py
+++ pre_processing.py
@@ -1,6 +1,6 @@
 import ast
 import re
-file =  open("./resources/DatasetDorn/dataset/snippets/python/5.jsnp" , 'r', encoding = "utf-8") 
+file =  open("./resources/DatasetDorn/dataset/snippets/python/10.jsnp" , 'r', encoding = "utf-8") 
 snippet1 = file.read()
 
 
@@ -17,6 +17,25 @@ def reg_number_of_loops(code) :
     else :
         return len(output)
 
+def number_of_indentation(code) : 
+    lines = code.split('\n')
+    indentation_level = 0
+
+    for line in lines:
+        match = re.match(r'^\s+', line)
+        if match:
+            indentation = match.group(0)
+            if indentation_level == 0 or len(indentation) > len(previous_indentation):
+                indentation_level += 1
+            elif len(indentation) < len(previous_indentation):
+                indentation_level -= 1
+                previous_indentation = indentation
+        else:
+            previous_indentation = ""
+
+    print(f"Indentation level: {indentation_level} - {line}")
+
 
 x = reg_number_of_loops(snippet1)
 print(x)
+number_of_indentation(snippet1)
